d5c049338cb2207f8017f270778c735b526c356f
huong2909 <huong>
2022-06-07 09:40:34 +0700

    baitap

---
+++ src/main/java/com/codegym/service/user/IUserService.java
@@ -0,0 +1,9 @@
+package com.codegym.service.user;
+
+import com.codegym.model.Users;
+import com.codegym.service.IGenericService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IUserService extends IGenericService<Users> {
+}
